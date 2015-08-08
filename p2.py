73be114804618cceb5b4f7e51b9dab99fb32a688
Manoj Gautam <surfer.manoj@gmail.com>
2015-08-08 12:05:54 +0545

    Updated

---
+++ p2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#Author: nix@1947
+#Description: P2
+count = 0
+sum = 0
+a = 0
+b = 1
+check = True
+while check:
+	c = a + b
+	if c >= 4000000:
+		check = False
+	if c % 2 == 0:
+		sum = sum + c
+	
+	a = b
+	b = c
+
+print sum
